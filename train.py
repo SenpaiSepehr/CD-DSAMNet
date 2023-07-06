69b940d59b3651eaf4123779ad2b11ddafdfe89b
SenpaiSepehr <sepehr2s1379@gmail.com>
2023-07-06 16:43:23 +0900

    fixed param saving dictionary

--- train.py
+++ train.py
@@ -55,6 +55,10 @@ if __name__ == '__main__':
     CDcriterion1 = DiceLoss().to(device, dtype=torch.float)
 
     results = {'train_loss': [], 'train_CT':[], 'train_Dice':[],'val_IoU': []}
+    
+    best_stat_dict = None
+    best_val_IoU = 0.0
+    best_epoch = 0
 
     # training
     for epoch in range(1, opt.num_epochs + 1):
@@ -67,8 +71,9 @@ if __name__ == '__main__':
 
             image1 = image1.to(device, dtype=torch.float)
             image2 = image2.to(device, dtype=torch.float)
-            label = label.to(device, dtype=torch.float) # one-hot label
-
+            label = label.to(device, dtype=torch.float) # one-hot label^
+            #(16,2,256,256)
+            
             prob, ds2, ds3 = netCD(image1, image2)
 
             #Diceloss
@@ -79,6 +84,8 @@ if __name__ == '__main__':
 
             # contrative loss
             label = torch.argmax(label, 1).unsqueeze(1).float()
+            #(16,1,256,256)
+
             CT_loss = CDcriterion(prob, label)  # BCL.py def_forward(distance,label)
 
             # CD loss
@@ -97,7 +104,6 @@ if __name__ == '__main__':
                 desc='[%d/%d] CD: %.4f  ' % (
                     epoch, opt.num_epochs, running_results['CD_loss'] / running_results['batch_sizes'],
                     ))
-        #print(f"train: label{label.size()}, prob{prob.size()}")
         
         # eval
         netCD.eval()
@@ -116,8 +122,7 @@ if __name__ == '__main__':
                 # (16,2,256,256)
 
                 prob, ds2, ds3 = netCD(image1, image2)
-                label = torch.argmax(label, 1).unsqueeze(1) #(16,256,256) without unsqueeze
-
+                label = torch.argmax(label, 1).unsqueeze(1)
                 # label size (16,1,256,256) , prob size (16,64,256,256)
                 # prob sizr explained in section C. Metric Module
 
@@ -127,30 +132,31 @@ if __name__ == '__main__':
                 prob = prob.cpu().detach().numpy()         #(16,64,256,256)
                 gt_value = gt_value.cpu().detach().numpy() #(16,1,256,256)
 
-                # gt_value = np.squeeze(gt_value) #(16,256,256)
-                result = np.squeeze(prob)       #(16,256,256))
+                #gt_value = np.squeeze(gt_value) #(16,256,256)
+                result = np.squeeze(prob)       #(16,64,256,256))
 
-                intr, unn = calMetric_iou(gt_value, result)  #flip arguments?
+                intr, unn = calMetric_iou(result, gt_value)  #flip arguments?
                 inter = inter + intr
                 unin = unin + unn
-
+                IoU = inter * 1.0 / unin
                 val_bar.set_description(
-                    desc='IoU: %.4f' % (inter * 1.0 / unin))
+                    desc='IoU: %.4f' % (IoU))
 
-            valing_results['IoU'] = inter * 1.0 / unin
+            valing_results['IoU'] = IoU
 
         # save model parameters
         val_loss = valing_results['IoU']
 
-        if val_loss > 0.5 or epoch==1:
-            mloss = val_loss
-            torch.save(netCD.state_dict(),  opt.model_dir+'netCD_epoch_%d.pth' % (epoch))
-
         results['train_loss'].append(running_results['CD_loss'] / running_results['batch_sizes'])
         results['train_CT'].append(running_results['CT_loss'] / running_results['batch_sizes'])
         results['train_Dice'].append(running_results['Dice_loss'] / running_results['batch_sizes'])
-
         results['val_IoU'].append(valing_results['IoU'])
+
+        if val_loss > 0.5 or epoch==1:
+            if val_loss > best_val_IoU:
+                best_val_IoU = val_loss
+                best_epoch = epoch
+                best_stat_dict = netCD.state_dict()
 
         if epoch % 1 == 0 :
             data_frame = pd.DataFrame(
@@ -158,3 +164,7 @@ if __name__ == '__main__':
                       'val_IoU': results['val_IoU']},
                 index=range(1, epoch + 1))
             data_frame.to_csv(opt.sta_dir, index_label='Epoch')
+
+    print(best_epoch)
+    if best_stat_dict is not None:
+        torch.save(best_stat_dict, opt.model_dir + 'best_netCD_epoch%d.pth' % (best_epoch))
